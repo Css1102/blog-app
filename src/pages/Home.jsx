019ed1ddcaf62b8ddf7b75b2ed08ceea13639ccf
Css1102 <sethischiranjeev@gmail.com>
2024-08-22 14:00:01 +0530

    sixth commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,14 +46,17 @@ function Home() {
               <h3 className='text-2xl font-prec text-rose-600 inline-block hover:text-primary shadow-md transition duration-200 cursor-pointer'><Link className='hover:text-black cursor-pointer drop-shadow-md'
               to='/login'>Login Here</Link></h3>
             </div>
-            <h3 className="text-3xl font-semibold relative left-[30%] font-playwrite text-slate-900 hover:text-primary transition duration-200 mt-20">ON THIS DAY:15/07/1938</h3>
-          <h4 className="mt-20 mr-10 text-xl font-medium text-slate-900 relative top-20 hover:text-primary transition duration-200">Fagg's unique double-hundreds
+            <h3 className="text-3xl font-semibold relative left-[30%] font-playwrite text-slate-900 hover:text-primary transition duration-200 mt-20">ON THIS DAY:22/08/2011</h3>
+          <h4 className="mt-20 mr-10 text-xl font-medium text-slate-900 relative top-20 hover:text-primary transition duration-200">England 4, India 0
+
 </h4>
-          <p className='text-sm font-[400] text-left w-full flex flex-row-reverse relative top-24 ml-0 font-Roboto text-rose-600 '>Kent's 23-year-old Arthur Fagg became the first man in history to score two double-centuries in a first-class match when he cracked 202 not out in two hours and 50 minutes against Essex in Colchester. He had already made a five-hour 244 on the first day, including a century before lunch. Less than 18 months earlier Fagg had returned early from the tour of Australia on a stretcher after contracting rheumatic fever, and subsequently missed the entire 1937 summer. Fagg remains one of only two players to achieve this rare feat - in February 2019, Sri Lanka's Nondescripts Cricket Club captain Angelo Perera became the second, making 201 and 231 in the same game against the Sinhalese Sports Club.
+          <p className='text-sm font-[400] text-left w-full flex flex-row-reverse relative top-24 ml-0 font-Roboto text-rose-600 '>England completed their first series whitewash in seven years when they won the Oval Test against India by an innings and eight runs. Ian Bell was India's chief tormentor, getting his first double-century in the course of a 350-run partnership with Kevin Pietersen. India made 300 for the first time in a series where the abjectness of their performance belied their No. 1 ranking, and that was solely due to an unbeaten century by Rahul Dravid, who carried his bat and came out again to open in the follow-on. When Sachin Tendulkar and Amit Mishra put on 144 on the last day, it looked like India might engineer an escape, but Tendulkar fell nine short of what would have been his 100th international hundred, after which five wickets were lost for 21 runs. Graeme Swann, who came late to the series party, took nine in the match.
+
+
 
 
 </p>
-     <img className='h-[50%] w-[50%] object-cover mt-32 relative left-64' src="https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1280,q_70/lsci/db/PICTURES/CMS/217700/217729.jpg" alt="" />        
+     <img className='h-[50%] w-[50%] object-cover mt-32 relative left-64' src="https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1280,q_70/lsci/db/PICTURES/CMS/136300/136346.jpg" alt="" />        
           </div>
         </Container>
       </div>
