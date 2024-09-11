6680f29f2eee7ff099fc7dc7119001d054233d30
Css1102 <sethischiranjeev@gmail.com>
2024-09-12 01:04:00 +0530

    eight commit b

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import blogimg from '../assets/blogimg.svg'
 function Logo() {
   return (
-    <img className='ml-0  h-[100%] w-[100%] ' src={blogimg} alt="Logo img" />
+    <img className='ml-0 h-[100%] w-[100%] ' src={blogimg} alt="Logo img" />
   )
 }
 
