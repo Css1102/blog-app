932304f9ad97ddbf0ed5d4362209defca56b478a
Css1102 <sethischiranjeev@gmail.com>
2024-09-12 19:09:33 +0530

    eigth commit d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,15 +20,27 @@ function Home() {
 
   if(posts.length!==0 && logoutChk){
     return (
-      <div className="w-full py-8">
+      <div className="w-full">
         <Container>
-          <div className="flex flex-wrap justify-around align-middle">
-            {posts.map((post) => (
+        <h2 className="md:text-3xl mb-20 text-base font-normal md:bottom-10 md:font-semibold relative md:left-0 font-playwrite text-slate-900 hover:text-primary transition duration-200 mt-20">TRIVIA</h2>
+        <div className="flex flex-col py-0 mt-0 justify-between">
+        <div className="mt-0">
+        <h3 className="md:text-3xl text-base font-normal md:font-semibold relative md:bottom-16 md:right-[440px] xl:right-[470px] font-playwrite text-slate-900 mb-6 hover:text-primary transition duration-200">Did you know?</h3>
+        <p className="text-sm md:font-[400] md:mt-0 text-left md:bottom-16 w-full flex flex-row-reverse relative ml-0 font-Roboto text-rose-600 ">
+        The lowest score ever defended in an ODI World Cup final, as of 2023, is 183 runs. This occurred during the 1983 Cricket World Cup final when India faced the West Indies. India batted first and was bowled out for 183 runs.West Indies motored to 50 for 1, with Viv Richards scything seven fours in a 28-ball 33, but the match turned when he hoicked Madan Lal high in the air and was superbly caught by Kapil Dev. The drama of 57 for 3 soon turned into a crisis at 76 for 6, as India's medium-pacers wobbled West Indies to death, and the cricket world slowly realised that a monstrous shock was on the cards. The Man of the Match was Mohinder Amarnath, for his 26 and 3 for 12. The highest score in the whole match was Kris Srikkanth's 38.
+
+Despite the low total, India managed to bowl out the West Indies for 140 runs, winning the match by 43 runs. This historic victory marked India's first World Cup win and is considered one of the greatest upsets in cricket history.It also changed the trajectory of cricket in India and the popularity of cricket in India skyrocketed after that. India's ability to defend such a low total in the final remains a remarkable achievement in the annals of the sport.
+        </p>
+        </div>
+        <img className='h-[50%] w-[50%] object-cover relative left-16 md:left-64' src="https://m.economictimes.com/thumb/msid-101246250,width-1600,height-900,resizemode-4,imgsize-67744/1983-world-cup-ians.jpg" alt="" />
+        </div>
+       <Link to='/allposts'><h4 className=" text-sm font-normal left-[0px] mt-8  xl:mr-60 xl:ml-10 xl:left-24  md:text-xl md:font-medium text-rose-700 relative hover:text-slate-600 transition duration-200 shadow-md cursor-pointer">Click here</h4></Link>
+       <h4 className=" text-sm font-normal left-[0px]  xl:mr-60 xl:ml-10 xl:left-24  md:text-xl md:font-medium text-slate-900 relative hover:text-primary transition duration-200">If you are also a cric nerd Like me</h4>
+            {/* {posts.map((post) => (
               <div className="p-2 w-1/4" key={post.$id}>
                 <PostCard {...post} />
               </div>
-            ))}
-          </div>
+            ))} */}
         </Container>
       </div>
     );
@@ -47,7 +59,7 @@ function Home() {
               to='/login'>Login Here</Link></h3>
             </div>
             <h3 className="md:text-3xl text-base font-normal md:font-semibold relative md:left-[30%] font-playwrite text-slate-900 hover:text-primary transition duration-200 mt-20">ON THIS DAY:11/09/2023</h3>
-          <h4 className=" mt-5 text-sm font-normal md:left-32 left-[16px] md:top-20 md:ml-10 xl:top-20 xl:mr-80 xl:ml-10  md:text-xl md:font-medium text-slate-900 relative hover:text-primary transition duration-200">Virat Kohli, KL Rahul and Kuldeep Yadav craft India's biggest win vs Pakistan
+          <h4 className="mt-5 text-sm font-normal md:left-32 left-[0px] md:top-20 md:ml-10 xl:top-20 xl:mr-80 xl:ml-10 xl:left-44  md:text-xl md:font-medium text-slate-900 relative hover:text-primary transition duration-200">Virat Kohli, KL Rahul and Kuldeep Yadav craft India's biggest win vs Pakistan
 
 
 
