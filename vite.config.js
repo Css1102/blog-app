15f424b2c278e3677a2bc8c152e35b069434f39d
Chiranjeev Singh Sethi <chiranjeev@Chiranjeevs-MacBook-Air.local>
2025-11-30 17:23:48 +0530

    jwt 9th commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc';
 import tailwindcss from 'tailwindcss'
-
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
